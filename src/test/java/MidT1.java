68d78b3c019545be56ae9181b8eb2d136c830882
yeonoo <ywoo818@gmail.com>
2014-10-24 12:05:20 +0900

    first

---
+++ src/test/java/MidT1.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by ce416-19 on 2014. 10. 24..
+ */
+public class MidT1 {
+    @Test
+    public void testMid1() throws Exception {
+        assertThat(8 * 18, is(144));
+    }
+}
